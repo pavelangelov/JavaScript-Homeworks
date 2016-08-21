90e182d0910d7e868d67cc2cc3f1fc9d236a7118
pavelangelov <p_a_v_el@abv.bg>
2016-08-21 12:29:58 +0300

    Added solutions

--- JavaScript-OOP/01.Functions-and-Function-Expressions/tasks/task-1.js
+++ JavaScript-OOP/01.Functions-and-Function-Expressions/tasks/task-1.js
@@ -7,8 +7,26 @@
 		throws if any of the elements is not convertible to Number	
 */
 
-function sum() {
-	
+function sum(numbers) {
+
+    if(Array.isArray(numbers) && numbers.length === 0) {
+        return null;
+    } else if(!numbers) {
+        throw new Error('Missing parameter!');
+    }
+
+	numbers.map(function(num) {
+        if(isNaN(+num)) {
+            throw new Error('Parameter is not convertible to Number!');
+        }
+    });
+
+    var result = 0;
+    numbers.map(function (n) {
+        result += (+n);
+      });
+
+    return result;
 }
 
 module.exports = sum;
--- JavaScript-OOP/01.Functions-and-Function-Expressions/tasks/task-2.js
+++ JavaScript-OOP/01.Functions-and-Function-Expressions/tasks/task-2.js
@@ -6,8 +6,41 @@
 		3) it must throw an Error if any of the range params is missing
 */
 
-function findPrimes() {
-	
+function findPrimes(startNumber, endNumber) {
+	if (typeof startNumber === 'undefined' || typeof endNumber === 'undefined') {
+        throw new Error('Missing parameter!');
+    } else if (isNaN(+startNumber) || isNaN(+endNumber)) {
+        throw new Error('Invalid parameters!');
+    }
+
+    startNumber = +startNumber;
+    endNumber = +endNumber;
+
+    var primeNumbers = [];
+    for (var i = startNumber; i <= endNumber; i += 1){
+        if (primeCheck(i)) {
+            primeNumbers.push(i);
+        }
+    }
+    
+
+    function primeCheck(number) {
+        var sqrt = Math.sqrt(number);
+
+        if (number < 2) {
+            return false;
+        }
+
+        for (var i = 2; i <= sqrt; i += 1){
+            if (number % i === 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    return primeNumbers;
 }
 
 module.exports = findPrimes;
