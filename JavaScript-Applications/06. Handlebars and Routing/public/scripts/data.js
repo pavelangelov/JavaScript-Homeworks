3449eccb757956e6e240c5c7eae7971028f19882
pavelangelov <p_a_v_el@abv.bg>
2016-09-29 12:29:13 +0300

    Finished refactoring.

--- JavaScript-Applications/06. Handlebars and Routing/public/scripts/data.js
+++ JavaScript-Applications/06. Handlebars and Routing/public/scripts/data.js
@@ -27,14 +27,57 @@ var data = (function () {
   }
 
   function threadsAdd(title) {
-    
+    return new Promise((resolve, reject) => {
+      let errMsg = "You must be logged to can create thread!",
+        errObj = {
+          responseText: JSON.stringify({ err: errMsg })
+        };
+
+      // if (!localStorage.getItem(USERNAME_STORAGE_KEY)) {
+      //   reject(errObj);
+      // } else {
+        let username = localStorage.getItem(USERNAME_STORAGE_KEY);
+        $.ajax({
+          url: 'api/threads',
+          method: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify({
+            title, username
+          }),
+          success: function (responce) {
+            resolve(responce);
+          }
+        });
+      //}
+    });
   }
 
   function threadById(id) {
-
+    return new Promise((resolve, reject) => {
+      let thread = $.getJSON('api/threads/' + id)
+        .done(resolve)
+        .fail(reject);
+    });
   }
 
   function threadsAddMessage(threadId, content) {
+    return new Promise((resolve, reject) => {
+      let username = localStorage.getItem(USERNAME_STORAGE_KEY),
+        body = {
+          content,
+          username
+        };
+
+      $.ajax({
+        url: `api/threads/${threadId}/messages`,
+        method: 'POST',
+        data: JSON.stringify(body),
+        contentType: 'application/json',
+        success: function (response) {
+          resolve(response);
+        }
+      });
+    });
 
   }
   // end threads
@@ -42,6 +85,11 @@ var data = (function () {
   // start gallery
   function galleryGet() {
     const REDDIT_URL = `https://www.reddit.com/r/aww.json?jsonp=?`;
+    return new Promise((resolve, reject) => {
+      $.getJSON(REDDIT_URL, function(json){
+        resolve(json);
+      });
+    });
     
   }
   // end gallery
--- JavaScript-Applications/06. Handlebars and Routing/public/scripts/router.js
+++ JavaScript-Applications/06. Handlebars and Routing/public/scripts/router.js
@@ -20,6 +20,10 @@ let sammyApp = Sammy('#content', function () {
 
     this.get('threads/:id', function(data) {
         let threadId = data.params.id;
+        console.log(data);
+        if (data.messages) {
+
+        }
         Promise.all([$.get(`api/threads/${threadId}`),$.get('../templates/thread-messages.handlebars')])
                 .then(([data, template]) => {
                     let thread = data.result;
@@ -27,40 +31,112 @@ let sammyApp = Sammy('#content', function () {
                     let temp = Handlebars.compile(template),
                         html = temp(thread);
 
-                    document.getElementById('content').innerHTML += html;
+                    document.getElementsByClassName('container-messages')[0].innerHTML = html;
                 });
+    });
+
+    this.get('gallery', function() {
+        data.gallery.get()
+            .then(function(result) {
+                let imgs = result.data.children;
+                $.get('../templates/gallery.handlebars', function(template) {
+                    let temp = Handlebars.compile(template),
+                        html = temp(imgs);
+                    
+                    $('#content').html(html);
+                });
+            });
     });
 });
 
 (function () {
     sammyApp.run('#/');
 
-    $('#content').on('click', '#btn-add-thread', function (e) {
-        let title = $(this).parents('.input-group')
-                            .find('input#input-add-thread').val() || null,
-            username = localStorage.getItem('username-key');
+    let GLYPH_UP = 'glyphicon-chevron-up',
+        GLYPH_DOWN = 'glyphicon-chevron-down',
+        root = $('#root'),
+        navbar = root.find('nav.navbar'),
+        contentContainer = $('#root #content'),
+        usernameInput = $('#login input'),
+        loginForm = $('#login'),
+        logoutForm = $('#logout'),
+        usernameSpan = $('#span-username'),
+        alertTemplate = $($('#alert-template').text());
+    
+    $('.navbar').on('click', '#btn-login', (ev) => {
+        let username = usernameInput.val() || 'anonymous';
 
-        if (username) {
-
-            let threadToAdd = {
-                title,
-                username
-            };
-
-            $.ajax({
-                url: 'api/threads',
-                method: 'POST',
-                contentType: 'application/JSON',
-                data: JSON.stringify(threadToAdd),
-                success: (res) => {
-                    $('#btn-threads').click();
-                }
-            });
-        }
+        data.users.login(username)
+        .then((user) => {
+            usernameInput.val('');
+            usernameSpan.text(user);
+            loginForm.addClass('hidden');
+            logoutForm.removeClass('hidden');
+            window.location = '#/';
+        });
     });
 
+    $('.navbar').on('click', '#btn-logout', (ev) => {
+        data.users.logout()
+        .then(() => {
+            usernameSpan.text('');
+            loginForm.removeClass('hidden');
+            logoutForm.addClass('hidden');
+            window.location = '#/';
+        });
+    });
+
+    $('#content').on('click', '#btn-add-thread', function (e) {
+        let title = $(this).parents('.input-group')
+                            .find('input#input-add-thread').val() || null;
+
+        data.threads.add(title)
+            .then(function() {
+                window.location = '#/threads';
+            });
+    });
+
+    $('#content').on('click', '.btn-add-message', function(ev) {
+        let $target = $(ev.target),
+        $container = $target.parents('.container-messages div.panel-default'),
+        thId = $container.attr('data-thread-id'),
+        msg = $container.find('.input-add-message').val();
+
+        data.threads.addMessage(thId, msg)
+            .then(function(thread) {
+                window.location = `#/threads/${thread.id}`;
+            });
+    });
+
+    function showMsg(msg, type, cssClass, delay) {
+    let container = alertTemplate.clone(true)
+        .addClass(cssClass).text(`${type}: ${msg}`)
+        .appendTo(root);
+
+    setTimeout(() => {
+      container.remove();
+    }, delay || 2000)
+  }
+
+  navbar.on('click', 'li', (ev) => {
+    let $target = $(ev.target);
+    $target.parents('nav').find('li').removeClass('active');
+    $target.parents('li').addClass('active');
+  });
+
+  contentContainer.on('click', '.btn-close-msg', (ev) => {
+    let msgContainer = $(ev.target).parents('.container-messages').remove();
+  });
+
+  contentContainer.on('click', '.btn-collapse-msg', (ev) => {
+    let $target = $(ev.target);
+    if ($target.hasClass(GLYPH_UP)) {
+      $target.removeClass(GLYPH_UP).addClass(GLYPH_DOWN);
+    } else {
+      $target.removeClass(GLYPH_DOWN).addClass(GLYPH_UP);
+    }
+
+    $target.parents('.container-messages').find('.messages').toggle();
+  });
+
 } ());
-
-
-
-export { sammyApp };
